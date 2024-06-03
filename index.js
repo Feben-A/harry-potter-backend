bb9ce804d248e43e32e14d5d8787fb7f246b7564
Feben-A <afa508165@gmail.com>
2024-06-03 17:07:51 +0100

    Add backend api code for get, post and delete

--- index.js
+++ index.js
@@ -1,14 +1,61 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port= process.env.PORT
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+//adding middleware
+app.use(express.json());
 
-app.listen(port, () =>{
-    console.log(`The app is listening on port ${port}`)
-})
+const characters = require("./harrypotter.json");
 
+app.get("/", function (req, res) {
+  res.send("Hello to the world of Harry Potter");
+});
 
+app.get("/characters", (req, res) => {
+  res.send(characters);
+});
+
+app.get("/characters/:id", (req, res) => {
+  const id = req.params.id;
+  const character = characters.find((character) => character.id == id);
+  if (character == undefined) {
+    res.status(404).send("The character does not exist!!");
+  } else res.send(character);
+});
+
+const ids = characters.map((character) => character.id);
+let maxId = Math.max(...ids);
+
+app.post("/characters", (req, res) => {
+  const character = characters.find(
+    (character) => character.name.toLowerCase() == req.body.name.toLowerCase()
+  );
+  if (character != undefined) {
+    res.status(409).send("This character already exists");
+  } else {
+    maxId += 1;
+    req.body.id = maxId;
+    characters.push(req.body);
+    res.status(201).send(req.body);
+  }
+});
+
+app.delete("/characters/:name", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  const characterIndex = characters.findIndex(
+    (character) => character.name.toLowerCase() == name
+  );
+  if (characterIndex == -1) {
+    res.status(404).send("The character doesn't exist");
+  } else {
+    characters.splice(characterIndex, 1);
+    res.sendStatus(204);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`The app is listening on port ${port}`);
+});
+
+app.post;
